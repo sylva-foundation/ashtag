8639c3dae5558f2bfc662bbe20b6382dd6633a2c
Steve Pike <steve.pike@carbonculture.net>
2013-06-17 09:48:33 +0100

    PEP-8 (fussy, I know...)

--- src/ashtag/apps/core/views.py
+++ src/ashtag/apps/core/views.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from registration.backends.simple.views import RegistrationView
 from registration.forms import RegistrationFormUniqueEmail
 
+
 class AshTagRegistrationView(RegistrationView):
 	form_class = RegistrationFormUniqueEmail
 	def get_success_url(self, request, user):
