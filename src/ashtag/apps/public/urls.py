b662adfe00670ee18bdbda64053e30d1ce3db004
Adam Charnock <adam@omniwiki.co.uk>
2013-06-17 13:18:26 +0100

    Initial work on getting public pages into the django app.

--- src/ashtag/apps/public/urls.py
+++ src/ashtag/apps/public/urls.py
@@ -3,4 +3,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('ashtag.apps.public.views',
     url('^$', TemplateView.as_view(template_name='public/home.html'), name='home'),
+    url('^about/$', TemplateView.as_view(template_name='public/about.html'), name='about'),
+    url('^privacy/$', TemplateView.as_view(template_name='public/privacy.html'), name='privacy'),
 )
