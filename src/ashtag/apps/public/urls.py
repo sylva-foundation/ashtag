c4bdd3e971ed437a2e20666a717c3dcb3b18c515
Adam Charnock <adam@omniwiki.co.uk>
2013-06-18 15:57:56 +0100

    Creating links in the menu

--- src/ashtag/apps/public/urls.py
+++ src/ashtag/apps/public/urls.py
@@ -4,5 +4,7 @@ from django.views.generic import TemplateView
 urlpatterns = patterns('ashtag.apps.public.views',
     url('^$', TemplateView.as_view(template_name='public/home.html'), name='home'),
     url('^about/$', TemplateView.as_view(template_name='public/about.html'), name='about'),
+    url('^contact/$', TemplateView.as_view(template_name='public/contact.html'), name='contact'),
     url('^privacy/$', TemplateView.as_view(template_name='public/privacy.html'), name='privacy'),
+    url('^terms/$', TemplateView.as_view(template_name='public/terms.html'), name='terms'),
 )
