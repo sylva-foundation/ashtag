c60a1ebf2ded669294980033157fa24377f140c4
Adam Charnock <adam@omniwiki.co.uk>
2013-07-08 18:53:01 +0100

    createthumbnails command now hides sightings which have no/erroneous images

--- src/ashtag/apps/core/management/commands/createthumbnails.py
+++ src/ashtag/apps/core/management/commands/createthumbnails.py
@@ -16,12 +16,18 @@ class Command(BaseCommand):
         total = Sighting.objects.count()
         counter = 1
         for sighting in Sighting.objects.all():
-            _log("Generating thumbnails for %d of %d ... " % (counter, total))
+            _log("Generating thumbnails for %d of %d (id: %s) ... " % (counter, total, sighting.pk))
             if sighting.image:
                 if create_thumbnails(sighting.image):
                     _log("done\n")
                 else:
-                    _log("ERRORS (logged)\n")
+                    _log("ERRORS (logged). Hiding sighting\n")
+                    self.hide(sighting)
             else:
-                _log("no image, skipping\n")
+                _log("no image, skipping. Hiding \n")
+                self.hide(sighting)
             counter += 1
+
+    def hide(self, sighting):
+        sighting.hidden = True
+        sighting.save()
