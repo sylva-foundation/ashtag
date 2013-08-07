11d665703af1fac5477316a962241c82dd574bae
Adam Charnock <adam@omniwiki.co.uk>
2013-08-07 20:21:52 +0100

    Fixing problem with some assets being unavailable offline

--- src/ashtag/settings/assets.py
+++ src/ashtag/settings/assets.py
@@ -14,6 +14,13 @@ PIPELINE_CSS = {
 }
 
 PIPELINE_JS = {
+    'jquery': {
+        'source_filenames': (
+            'js/lib/jquery-1.9.1.min.js',
+            'js/lib/jquery.mobile-1.3.1.min.js',
+        ),
+        'output_filename': 'js/jquery.js',
+    }
     'commonjs': {
         'source_filenames': (
             'js/jquery.min.js',
