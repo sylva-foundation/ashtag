d6fefb33dac38b4a3f07d6118a6e7b41e9925701
Adam Charnock <adam@omniwiki.co.uk>
2014-03-05 13:51:23 +0000

    Disabling querystring auth for static files

--- src/ashtag/settings/base.py
+++ src/ashtag/settings/base.py
@@ -295,6 +295,7 @@ MANIFESTO_FILTER_EXCLUDE_PATTERNS = [
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', None)
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', None)
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', 'ashtag-localdev')
+AWS_QUERYSTRING_AUTH = False
 AWS_HEADERS = {
     'Cache-Control': 'max-age=31536000, public',
 }
