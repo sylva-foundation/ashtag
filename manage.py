1cdcab162e9d13e5c792fc983137ef6c232adb88
Adam Charnock <adam@omniwiki.co.uk>
2015-05-22 17:01:37 +0100

    Updating config for heroku

--- manage.py
+++ manage.py
@@ -7,11 +7,6 @@ if __name__ == "__main__":
     sys.path.append(path(__file__).dirname() / 'src')
     sys.path.append(path(__file__).dirname() / 'lib')
 
-    if 'CRED_FILE' in os.environ:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ashtag.settings.live')
-    else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ashtag.settings.localdev')
-
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
--- wsgi.py
+++ wsgi.py
@@ -5,11 +5,6 @@ from path import path
 sys.path.append(path(__file__).dirname() / 'src')
 sys.path.append(path(__file__).dirname() / 'lib')
 
-if 'CRED_FILE' in os.environ:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ashtag.settings.live')
-else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ashtag.settings.localdev')
-
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 application = Cling(get_wsgi_application())
