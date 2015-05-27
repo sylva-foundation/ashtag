b4b7e505f50e84a5ae511525279792c7d1537dcd
Adam Charnock <adam@omniwiki.co.uk>
2015-05-27 16:35:29 +0100

    Setting postgis db engine in live config

--- src/ashtag/settings/live.py
+++ src/ashtag/settings/live.py
@@ -5,6 +5,7 @@ TEMPLATE_DEBUG = DEBUG
 
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
